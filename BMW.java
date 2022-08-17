02b30291a933dcf05afca86dc3ef774155080760
ashish4907 <75926126+ashish4907@users.noreply.github.com>
2022-08-17 18:56:44 +0530

    Add files via upload

--- BMW.java
+++ BMW.java
@@ -1,27 +1,10 @@
-import java.util.Comparator;
 
-public class BMW implements Comparator<Object>{
-	String model;
 
-	public BMW(String model) {
-		this.model = model;
-	}
 
-	public BMW() {
+public class BMW {
 	
-	}
-
-	@Override
-	public String toString() {
-		return "BMW [model=" + model + "]";
-	}
-
-	@Override
-	public int compare(Object o1, Object o2) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 	
 	
 
 }
+
--- MainRunner.java
+++ MainRunner.java
@@ -1,21 +1,51 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainRunner {
-public static void main(String[] args) {
-	
-	Add add =(int a) ->
-	System.out.println(a*a);
-	add.add(10);
-	
-	Add adds = a -> System.out.println(a*a);
-	adds.add(12);
-	
+	public static void main(String[] args) {
 
-	AddParameter addpar = a -> {
-		System.out.println(a*a);
-		return a;
+		Object[] objlist = { new BMW(), new Audi(), new BMW(), new Audi() };
+
+		System.out.println("-----using arrays-----");
+
+		for (Object obj : objlist) {
+			if (obj instanceof BMW) {
+
+				System.out.println("Belongs TO BMW");
+
+			} else if (obj instanceof Audi) {
+
+				System.out.println("Belongs TO Audi");
+			}
+		}
+		System.out.println("-----using collection-----");
+		List<Object> al = Arrays.asList(objlist);
+		List<BMW> bmw = new ArrayList<>();
+		List<Audi> audi = new ArrayList<>();
+		for (Object obj : al) {
+			if (obj instanceof BMW) {
+				bmw.add((BMW) obj);
+				System.out.println("Belongs TO BMW");
+			} else if (obj instanceof Audi) {
+
+				audi.add((Audi) obj);
+				System.out.println("Belongs TO Audi");
+			}
+		}
+		System.out.println("-----Iterating BMW-----");
+		Iterator<BMW> iter1 = bmw.iterator();
+		while(iter1.hasNext()) {
+			System.out.println(iter1.next());
+		}
+		System.out.println("-----Iterating Audi-----");
 		
-	};
-	addpar.add(11);
-	System.out.println(addpar);
-}
+		Iterator<Audi> iter2 = audi.iterator();
+		while(iter2.hasNext()) {
+			System.out.println(iter2.next());
+		}
+
+	}
+
 }
